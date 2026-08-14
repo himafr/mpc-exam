3069514ee725e40b2ad4e8c08731d3b29ec4fad0
hima <ebrahumahmed2003@gmail.com>
2026-08-14 17:30:54 +0300

    feat: add Docker support and update package scripts
    - Added Dockerfile for containerization of the application.
    - Created docker-compose.yml for managing application services.
    - Updated package.json to include new scripts for starting, developing, and managing Docker containers.
    - Removed health test file as it is no longer needed.

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
-  testTimeout: 20000,
-};
